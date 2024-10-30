f4bda481ca4ef4c56e56a5988d1dd5fdbb456c04
silkeholmebonnen <silke.bonnen@gmail.com>
2024-10-30 14:35:07 +0100

    working

--- graphlib/__init__.py
+++
@@ -1,7 +0,0 @@
-# Importing core classes and functions to make them accessible directly from graphlib package
-
-from .graph import Graph, Node, Edge
-from .utils import create_graph
-
-# Setting what gets exposed when doing "from graphlib import *"
-__all__ = ["Graph", "Node", "Edge", "create_graph"]
